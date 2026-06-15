476c62450c869e0e11856d5c4d056be86728f1c4
Sasha Di Figlia <difiglia95@hotmail.it>
2026-06-15 12:55:39 +0200

    wip: weapon

--- Assets/Scripts/ScriptableObjects/Weapon.cs
+++ Assets/Scripts/ScriptableObjects/Weapon.cs
@@ -3,5 +3,12 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Weapon", menuName = "Equipment/New Weapon")]
 public class Weapon : ScriptableObject
 {
-    
+    public float Damage;
+    public float Range;
+    public float MaxDurability;
+
+    public bool Attack()
+    {
+        return false;
+    }
 }
