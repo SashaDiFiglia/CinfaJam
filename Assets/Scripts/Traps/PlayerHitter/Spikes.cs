67d73fe72b41f7497268f51a4995aeac89bea84c
Beatrice Verardi <shinybea01@gmail.com>
2026-06-15 11:21:30 +0200

    Spike activation/behavior

---
+++ Assets/Scripts/Traps/PlayerHitter/Spikes.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour
+{
+    private float _damage;
+    private float _delay;
+
+    private bool _canHit;
+    private Coroutine _hitDelayC;
+    
+    
+    public void Setup(float value, float delayBetweenHits)
+    {
+        _damage = value;
+        _delay = delayBetweenHits;
+        _canHit = true;
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        ClearCoroutine();
+        _canHit = true;
+        Hit(other.gameObject);
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (!_canHit) return;
+        Hit(other.gameObject);
+    }
+
+    void Hit(GameObject other)
+    {
+        Debug.Log($"Spikes hit {other.name} for {_damage} damage");
+        _canHit = false;
+        StartCoroutine(Delay());
+    }
+
+    private IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(_delay);
+        _canHit = true;
+    }
+    private void ClearCoroutine()
+    { if (_hitDelayC != null) { StopCoroutine(_hitDelayC); _hitDelayC = null; } }
+
+}
--- Assets/Scripts/Traps/TrapsSos/ActivableFloorSpikesTrap.cs
+++ Assets/Scripts/Traps/TrapsSos/ActivableFloorSpikesTrap.cs
@@ -4,25 +4,30 @@ using UnityEngine.UI;
 [CreateAssetMenu(fileName = "New Activable Spikes", menuName = "Traps/Activable Floor - Spikes")]
 public class ActivableFloorSpikesTrap: AActivableTrap
 {
+    [SerializeField] private float damage;
+    [SerializeField] private float delayBetweenHits;
     [SerializeField] private Sprite inactiveSprite;
     [SerializeField] private Sprite activeSprite;
     [SerializeField] private GameObject colliderObj;
-    private GameObject _currentCollider;
+    private GameObject _currentCol;
 
     protected override void OnSetup()
-    { _currentCollider = Instantiate(colliderObj, trapController.transform.position, Quaternion.identity); }
+    {
+        _currentCol = Instantiate(colliderObj, trapController.transform.position, Quaternion.identity);
+        _currentCol.GetComponent<Spikes>().Setup(damage, delayBetweenHits);
+    }
     protected override void OnClear()
-    { Destroy(_currentCollider); _currentCollider = null; }
+    { Destroy(_currentCol); _currentCol = null; }
 
     public override void ActivateTrap()
     {
         trapController.GetComponent<Image>().sprite = activeSprite;
-        _currentCollider.SetActive(true);
+        _currentCol.SetActive(true);
     }
 
     public override void DeactivateTrap()
     {
         trapController.GetComponent<Image>().sprite = inactiveSprite;
-        _currentCollider.SetActive(false);
+        _currentCol.SetActive(false);
     }
 }
