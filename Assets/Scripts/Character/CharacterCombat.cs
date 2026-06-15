560928e9d389d7efa137f716cfcf04b4ac902f26
Sasha Di Figlia <difiglia95@hotmail.it>
2026-06-15 13:33:46 +0200

    wip: combat

--- Assets/Scripts/Character/CharacterCombat.cs
+++ Assets/Scripts/Character/CharacterCombat.cs
@@ -1,6 +1,7 @@
 using System;
 using FMOD.Studio;
 using FMODUnity;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 public class CharacterCombat : MonoBehaviour
@@ -31,7 +32,7 @@ public class CharacterCombat : MonoBehaviour
     private EventInstance _attackSoundInstance;
     private EventInstance _weaponBreakInstance;
 
-    private float _currentDurability;
+    [ShowInInspector, ReadOnly] private float _currentDurability;
 
     public float CurrentDurability => _currentDurability;
 
@@ -63,7 +64,7 @@ public class CharacterCombat : MonoBehaviour
 
         _attackSoundInstance.start();
 
-        if (!Weapon.Attack())
+        if (!Weapon.Attack(transform))
         {
             return;
         }
@@ -77,6 +78,14 @@ public class CharacterCombat : MonoBehaviour
             OnWeaponBreak?.Invoke();
 
             Weapon = null;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (Weapon != null)
+        {
+            Weapon.DrawGizmos(transform);
         }
     }
 }
