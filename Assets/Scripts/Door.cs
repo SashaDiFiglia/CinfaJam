beecf00077692674dad627579b3973a8c994bdfe
Nicholas Comparoni <38912282+NicholasComparoni@users.noreply.github.com>
2026-06-19 10:25:10 +0200

    Door Update

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,13 +1,28 @@
-﻿using Unity.Burst.Intrinsics;
+﻿using System;
+using Unity.Burst.Intrinsics;
 using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    [SerializeField] private Collider _col;
     
+    private Collider _col;
+    private SpriteRenderer _renderer;
     
-    
-    
-    
-    
+    [Header("Open Sprite")]
+    [SerializeField] private Sprite _openSprite;
+
+
+    private void Awake()
+    {
+        _col= GetComponent<Collider>();
+        _renderer = GetComponent<SpriteRenderer>();
+    }
+
+
+
+    public void Open()
+    {
+        _renderer.sprite = _openSprite;
+        _col.enabled = false;
+    }
 }
