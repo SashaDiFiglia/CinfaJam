5308cd91f8c5260f56f574f6531a999a7b7e4e15
Nicholas Comparoni <38912282+NicholasComparoni@users.noreply.github.com>
2026-06-19 10:09:41 +0200

    Door and Door Manager

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,13 @@
+﻿using Unity.Burst.Intrinsics;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] private Collider _col;
+    
+    
+    
+    
+    
+    
+}
---
+++ Assets/Scripts/DoorManager.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorManager : MonoBehaviour
+{
+    [SerializeField] private List<BracierInstance> braciers;
+    //[SerializeField] private List<Enemies> enemies; 
+    [SerializeField] private List<Door> doors;
+
+
+
+    private void Awake()
+    {
+        
+    }
+
+
+    private void Update()
+    {
+        
+    }
+    
+}
