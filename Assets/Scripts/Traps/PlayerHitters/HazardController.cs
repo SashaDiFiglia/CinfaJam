4a56055e63c1a58df809119e043aff2c97ad3e00
Beatrice Verardi <shinybea01@gmail.com>
2026-06-22 10:53:10 +0200

    Fixed hazard collision stay (hazardController)

--- Assets/Scripts/Traps/PlayerHitters/HazardController.cs
+++ Assets/Scripts/Traps/PlayerHitters/HazardController.cs
@@ -47,7 +47,7 @@ public class HazardController : MonoBehaviour
         if (_anim != null) { MoveSpikes(activating); }
         _col.enabled = activating;
     }
-
+    
     #region Animator - Move Spikes
         private void MoveSpikes(bool isForward)
         {
@@ -92,7 +92,7 @@ public class HazardController : MonoBehaviour
             if (other.GetComponent<CharacterHealth>() == null) { return; }
             ClearCoroutine(); Hit(other.gameObject);
         }
-        private void OnTriggerStay(Collider other)
+        private void OnTriggerStay2D(Collider2D other)
         {
             if (!_dealsDamageOverTime) { return; }
             if (other.GetComponent<CharacterHealth>() == null) { return; }
@@ -111,7 +111,7 @@ public class HazardController : MonoBehaviour
         {
             _canHit = false;
             player.GetComponent<CharacterHealth>()?.TakeDamage(_damage);
-            Debug.Log($"Spikes hit {player.name} for {_damage} damage");
+            // Debug.Log($"Hit {player.name} for {_damage} damage");
             StartCoroutine(Delay());
         }
 
@@ -123,7 +123,10 @@ public class HazardController : MonoBehaviour
         private IEnumerator Delay()
         { yield return new WaitForSeconds(_delay); _canHit = true; }
         private void ClearCoroutine()
-        { if (_hitDelayC != null) { StopCoroutine(_hitDelayC); _hitDelayC = null; _canHit = true; } }
+        {
+            if (_hitDelayC != null) { StopCoroutine(_hitDelayC); _hitDelayC = null; }
+            _canHit = true; 
+        } 
     #endregion
     
 }
