c5bc84bff6c49f34d874036a0969f5f97db166d3
Nicholas Comparoni <nicholas.compa@gmail.com>
2026-06-22 13:02:03 +0200

    Pusho un attimo pusho pusho uin attimo pusho pusho un attimo

--- Assets/Scripts/EndGame.cs
+++ Assets/Scripts/EndGame.cs
@@ -5,7 +5,7 @@ public class EndGame : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.GetComponent<CharacterController>())
+        if (other.GetComponent<CharacterMovement>())
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
